f3f43a17708b1209c734e2aea713e204e75551f4
mauricioohse <mauricioohsexd@hotmail.com>
2023-08-19 20:26:28 -0300

    i think i resolved the hasComponent() compilation issue

--- BirchEngine/Src/ECS/ColliderComponent.h
+++ BirchEngine/Src/ECS/ColliderComponent.h
@@ -19,10 +19,10 @@ public:
 
 	void init() override
 	{
-		//if (!entity->hasComponent<TransformComponent>())
-		//{
-		//	entity->addComponent<TransformComponent>();
-		//}
+		if (!entity->hasComponent<TransformComponent>())
+		{
+			entity->addComponent<TransformComponent>();
+		}
 		transform = &entity->getComponent<TransformComponent>();
 	}
 
