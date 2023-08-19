b8c0d00a326dc3ff18cfa8ae93d7848c8238fdb4
mauricioohse <41490478+mauricioohse@users.noreply.github.com>
2023-08-19 20:28:52 -0300

    Video 11 collision detection (#6)
    * first collision implementation, no test yet
    
    * for some random reason now there is no more hasComponent() compilation errors. No fucking idea why but hey, it works!
    
    * ok, last commit was a fluke - the hasComponent bug still exists, but i had forgot to save the file. at least the collision detection is working, but the hasComponent() still has some issues.
    
    * i think i resolved the hasComponent() compilation issue
    
    * removing sdl2 and sdl2_image folders, will do in a different commit

--- BirchEngine/Src/ECS/SpriteComponent.h
+++ BirchEngine/Src/ECS/SpriteComponent.h
@@ -2,6 +2,7 @@
 
 #include "Components.h"
 #include "SDL.h"
+#include "../TextureManager.h"
 
 class SpriteComponent : public Component
 {
@@ -14,7 +15,12 @@ public:
 	SpriteComponent() = default;
 	SpriteComponent(const char* path)
 	{
-		texture = TextureManager::LoadTexture(path);
+		SetTex(path);
+	}
+
+	~SpriteComponent()
+	{
+		SDL_DestroyTexture(texture);
 	}
 
 	void init() override
@@ -22,8 +28,8 @@ public:
 		transform = &entity->getComponent<TransformComponent>();
 		
 		srcRect.x = srcRect.y = 0;
-		srcRect.w = 16; // in the future, this has to be changed based on the particular asset/png size.
-		srcRect.h = 64;
+		srcRect.w = transform->width; // in the future, this has to be changed based on the particular asset/png size.
+		srcRect.h = transform->height;
 		destRect.w = srcRect.w * 2;
 		destRect.h = srcRect.h * 2;
 
@@ -33,6 +39,8 @@ public:
 	{
 		destRect.x = (int)transform->position.x;
 		destRect.y = (int)transform->position.y;
+		destRect.w = transform->width * transform->scale;
+		destRect.h = transform->height * transform->scale;
 	}
 
 	void draw() override
--- BirchEngine/Src/Map.cpp
+++ BirchEngine/Src/Map.cpp
@@ -37,6 +37,13 @@ Map::Map()
 	src.w = dest.w = 32;
 }
 
+Map::~Map()
+{
+	SDL_DestroyTexture(grass);
+	SDL_DestroyTexture(dirt);
+	SDL_DestroyTexture(water);
+}
+
 void Map::LoadMap(int arr[20][25])
 {
 	for (int row = 0; row < 20; row++)
